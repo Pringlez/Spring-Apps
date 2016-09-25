361c07e670a263166cd1726378643ad553ef03fb
John Walsh <john.walsh16609@gmail.com>
2016-09-25 18:51:12 +0100

    Update to Web Apps
    Update to web apps project
    Testing examples, and home controller setup
    
    Updated dependencies in pom files

---
+++ Web-Apps-Project/src/ie/walsh/springwebapps/config/RootConfig.java
@@ -0,0 +1,14 @@
+package ie.walsh.springwebapps.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan(basePackages={"ie.walsh.springwebapps"},excludeFilters={
+@Filter(type=FilterType.ANNOTATION, value=EnableWebMvc.class)})
+public class RootConfig {
+	
+}
