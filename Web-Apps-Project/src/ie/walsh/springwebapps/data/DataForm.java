ae0f0632fffb197d31d5015c5887f393bc5d4809
John Walsh <john.walsh16609@gmail.com>
2016-09-26 16:06:51 +0100

    Update to Web App Project
    Update to dependencies required for testing controllers
    More work required on controllers..

--- Web-Apps-Project/src/ie/walsh/springwebapps/data/DataForm.java
+++ Web-Apps-Project/src/ie/walsh/springwebapps/data/DataForm.java
@@ -1,22 +1,22 @@
 package ie.walsh.springwebapps.data;
 
-/*import javax.validation.constraints.Max;
+import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;*/
+import javax.validation.constraints.Size;
 
 public class DataForm {
 	
-	//@NotNull
-	//@Size(min=1, max=140)
+	@NotNull
+	@Size(min=1, max=140)
 	private String message;
 	  
-	//@Min(-180)
-	//@Max(180)
+	@Min(-180)
+	@Max(180)
 	private Double longitude;
 	  
-	//@Min(-90)
-	//@Max(90)
+	@Min(-90)
+	@Max(90)
 	private Double latitude;
 
 	public String getMessage() {
---
+++ Web-Apps-Project/target/spring-examples-0.0.3-SNAPSHOT/WEB-INF/views/example.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+  <head>
+    <title>Example</title>
+    <link rel="stylesheet" 
+          type="text/css" 
+          href="<c:url value="/resources/style.css" />" >
+  </head>
+  <body>
+    <div class="exampleView">
+      <div class="exampleMessage"><c:out value="${example.message}" /></div>
+      <div>
+        <span class="exampleTime"><c:out value="${example.time}" /></span>
+      </div>
+    </div>
+  </body>
+</html>
